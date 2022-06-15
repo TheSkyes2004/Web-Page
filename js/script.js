c60012a9850ca4ab0ef798e09c3fa16c221ae3cf
TheSkyes2004 <theskyesGaming@outlook.com>
2022-06-15 10:49:53 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========================================= typing animation ========================================= */
+var typed = new Typed(".typing", {
+    strings:["","IT Engineer","Coder","Programmer","Java Scripter"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
